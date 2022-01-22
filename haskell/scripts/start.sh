c0109b0fa772220361eb650fc9764961e89709a9
Florian Ritterhoff <florian@ritterhoff.org>
2022-01-22 06:34:31 +0100

    Start providing multiple images

---
+++ haskell/scripts/start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+CURRENT_RELEASE="1"
+CODE_SETTINGS=~/.local/share/code-server/User/settings.json
+INSTALLED_RELEASE="0"
+if [ -f  ~/.local/fingerprint ] ; then
+    INSTALLED_RELEASE=$(cat ~/.local/fingerprint)
+fi
+
+if [ $INSTALLED_RELEASE -lt 1 ]; then
+    mkdir -p /home/jovyan/.local/share/code-server/User/globalStorage/haskell.haskell
+    SETTINGS=".\"files.exclude\".\"**/.*/\" = true | .\"telemetry.enableTelemetry\" = false"
+    if [ -e $CODE_SETTINGS ] 
+    then
+        cat $CODE_SETTINGS | jq "$SETTINGS" > $CODE_SETTINGS.tmp
+        mv $CODE_SETTINGS.tmp $CODE_SETTINGS
+    else
+        mkdir -p ~/.local/share/code-server/User
+        jq -n "$SETTINGS" > $CODE_SETTINGS
+    fi
+    stack config set system-ghc --global true
+fi
+
+echo "$CURRENT_RELEASE" > ~/.local/fingerprint
