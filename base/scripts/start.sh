259e587bbd49b73e3d0d4199abef73dfb5776122
Florian Ritterhoff <32478819+fritterhoff@users.noreply.github.com>
2022-03-23 07:52:50 +0100

    chore: Bump versions

--- base/scripts/start.sh
+++ base/scripts/start.sh
@@ -7,7 +7,7 @@ uninstall_jupyter (){
     rm -rf ~/.local/share/code-server/extensions/ms-toolsai.jupyter*/
 }
 
-CURRENT_RELEASE="3"
+CURRENT_RELEASE="4"
 CODE_SETTINGS=~/.local/share/code-server/User/settings.json
 INSTALLED_RELEASE="0"
 if [ -f  ~/.local/fingerprint ] ; then
@@ -27,15 +27,21 @@ if [ $INSTALLED_RELEASE -lt 1 ]; then
 fi
 
 if [ $INSTALLED_RELEASE -lt 2 ]; then
-    curl -sfLO https://github.com/hm-riscv/vscode-riscv-venus/releases/download/v1.6.0/hm.riscv-venus-1.6.0.vsix
-    code-server --install-extension ./hm.riscv-venus-1.6.0.vsix
+    curl -sfLO https://github.com/hm-riscv/vscode-riscv-venus/releases/download/v1.6.0/hm.riscv-venus-1.7.0.vsix
+    code-server --install-extension ./hm.riscv-venus-1.7.0.vsix
     code-server --install-extension /opt/extensions/zhwu95.riscv-0.0.8.vsix
-    rm hm.riscv-venus-1.6.0.vsix
+    rm hm.riscv-venus-1.7.0.vsix
 fi
 
 # Check that the installed release is less than 2 and not 0
 if [ $INSTALLED_RELEASE -lt 3 ] && [ $INSTALLED_RELEASE -ne 0 ]; then
     uninstall_jupyter
+fi
+
+if [ $INSTALLED_RELEASE -lt 4 ] && [ $INSTALLED_RELEASE -ne 0 ]; then
+    curl -sfLO https://github.com/hm-riscv/vscode-riscv-venus/releases/download/v1.6.0/hm.riscv-venus-1.7.0.vsix
+    code-server --install-extension ./hm.riscv-venus-1.7.0.vsix    
+    rm hm.riscv-venus-1.7.0.vsix
 fi
 
 echo "$CURRENT_RELEASE" > ~/.local/fingerprint
