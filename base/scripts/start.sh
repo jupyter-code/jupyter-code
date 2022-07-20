55cfa79b792f5f4e197c96fbeccf28b1a0d2b972
Florian Ritterhoff <32478819+fritterhoff@users.noreply.github.com>
2022-07-20 05:04:01 +0000

    chore: bump versions

--- base/scripts/start.sh
+++ base/scripts/start.sh
@@ -27,7 +27,7 @@ if [ $INSTALLED_RELEASE -lt 1 ]; then
 fi
 
 if [ $INSTALLED_RELEASE -lt 2 ]; then
-    curl -sfLO https://github.com/hm-riscv/vscode-riscv-venus/releases/download/v1.6.0/hm.riscv-venus-1.7.0.vsix
+    curl -sfLO https://github.com/hm-riscv/vscode-riscv-venus/releases/download/v1.7.0/hm.riscv-venus-1.7.0.vsix
     code-server --install-extension ./hm.riscv-venus-1.7.0.vsix
     code-server --install-extension /opt/extensions/zhwu95.riscv-0.0.8.vsix
     rm hm.riscv-venus-1.7.0.vsix
@@ -39,7 +39,7 @@ if [ $INSTALLED_RELEASE -lt 3 ] && [ $INSTALLED_RELEASE -ne 0 ]; then
 fi
 
 if [ $INSTALLED_RELEASE -lt 4 ] && [ $INSTALLED_RELEASE -ne 0 ]; then
-    curl -sfLO https://github.com/hm-riscv/vscode-riscv-venus/releases/download/v1.6.0/hm.riscv-venus-1.7.0.vsix
+    curl -sfLO https://github.com/hm-riscv/vscode-riscv-venus/releases/download/v1.7.0/hm.riscv-venus-1.7.0.vsix
     code-server --install-extension ./hm.riscv-venus-1.7.0.vsix    
     rm hm.riscv-venus-1.7.0.vsix
 fi
