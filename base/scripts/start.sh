a28d5e723f620b9d950026de389d68b26ed359bc
Florian Ritterhoff <32478819+fritterhoff@users.noreply.github.com>
2022-07-21 12:24:44 +0200

    fix: use correct download links

--- base/scripts/start.sh
+++ base/scripts/start.sh
@@ -11,8 +11,8 @@ CODE_SETTINGS=~/.local/share/code-server/User/settings.json
 INSTALLED_RELEASE="0"
 
 patch_settings(){
-    SETTINGS=".\"files.exclude\".\"**/.*/\" = true | .\"telemetry.enableTelemetry\" = false | .\"python.defaultInterpreterPath\" = \"/opt/conda/bin/python\" | .\"security.workspace.trust.enabled\" = false" 
-    if [ -e $CODE_SETTINGS ] 
+    SETTINGS=".\"files.exclude\".\"**/.*/\" = true | .\"telemetry.enableTelemetry\" = false | .\"python.defaultInterpreterPath\" = \"/opt/conda/bin/python\" | .\"security.workspace.trust.enabled\" = false"
+    if [ -e $CODE_SETTINGS ]
     then
         cat $CODE_SETTINGS | jq "$SETTINGS" > $CODE_SETTINGS.tmp
         mv $CODE_SETTINGS.tmp $CODE_SETTINGS
@@ -31,10 +31,10 @@ if [ $INSTALLED_RELEASE -lt 1 ]; then
 fi
 
 if [ $INSTALLED_RELEASE -lt 2 ]; then
-    curl -sfLO https://github.com/hm-riscv/vscode-riscv-venus/releases/download/v1.7.0/hm.riscv-venus-1.7.0.vsix
-    code-server --install-extension ./hm.riscv-venus-1.7.0.vsix
+    curl -sfLO https://github.com/hm-riscv/vscode-riscv-venus/releases/download/v1.7.0/riscv-venus-1.7.0.vsix
+    code-server --install-extension ./riscv-venus-1.7.0.vsix
     code-server --install-extension /opt/extensions/zhwu95.riscv-0.0.8.vsix
-    rm hm.riscv-venus-1.7.0.vsix
+    rm riscv-venus-1.7.0.vsix
 fi
 
 # Check that the installed release is less than 2 and not 0
@@ -43,9 +43,9 @@ if [ $INSTALLED_RELEASE -lt 3 ] && [ $INSTALLED_RELEASE -ne 0 ]; then
 fi
 
 if [ $INSTALLED_RELEASE -lt 4 ] && [ $INSTALLED_RELEASE -ne 0 ]; then
-    curl -sfLO https://github.com/hm-riscv/vscode-riscv-venus/releases/download/v1.7.0/hm.riscv-venus-1.7.0.vsix
-    code-server --install-extension ./hm.riscv-venus-1.7.0.vsix    
-    rm hm.riscv-venus-1.7.0.vsix
+    curl -sfLO https://github.com/hm-riscv/vscode-riscv-venus/releases/download/v1.7.0/riscv-venus-1.7.0.vsix
+    code-server --install-extension ./riscv-venus-1.7.0.vsix
+    rm riscv-venus-1.7.0.vsix
 fi
 
 if [ $INSTALLED_RELEASE -lt 5 ] && [ $INSTALLED_RELEASE -ne 0 ]; then
